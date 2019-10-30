743291432a0ecaf36ee8b890ea42434214b7abd7
John Tambi <jon.tambi@gmail.com>
2019-10-30 12:50:24 -0500

    Add installation Jenkins Server

--- main.tf
+++ main.tf
@@ -132,13 +132,25 @@ resource "aws_instance" "webserver" {
     tags = {
         Name = "jenkins-demo"
     }
+
+    provisioner "remote-exec" {
+        inline = [
+
+            "sudo yum install java-1.8.0-openjdk-devel",
+            "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
+            "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
+            "sudo yum install jenkins",
+            "sudo yum update -y",
+            "sudo systemctl start jenkins",
+            "sudo systemctl enable jenkins",
+            "sudo cat /var/lib/jenkins/secrets/initialAdminPassword",
+        ]
+    }
 }
 
 output "public_ip" {
     value = aws_eip.ip_server.public_ip
 }
-# Continuar con aws_key_pair
-
 
 ### AMI CENTOS7 ami-02eac2c0129f6376b
 
